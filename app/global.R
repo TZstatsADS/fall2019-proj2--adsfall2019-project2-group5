c7bbf95f1433707c3060f96dca4c0481f109b1b3
zzzaaannn <54826490+zzzaaannn@users.noreply.github.com>
2019-10-02 08:42:52 -0400

    only did a little cleaning so far

--- app/global.R
+++ app/global.R
@@ -1,30 +1,38 @@
 
 tree <- read.csv("tree.csv",header = TRUE)
-tree <- tree %>% select(c("status","health","spc_common",
+
+data <- tree %>% select(c("status","health","spc_common",
                           "steward","guards","sidewalk",
-                          "address","zipcode","zip_city","boroname","Latitude","longitude"))
-
-problem <- tree %>% 
-
-count_df <- tree %>% 
+                          "address","zipcode","zip_city",
+                          "boroname","Latitude","longitude"))
+# trees count by region
+count_df <- data %>% 
   filter(zipcode>0) %>% 
   mutate(region = as.character(zipcode)) %>% 
   group_by(region) %>% summarise(value = n())
 
+head(count_df)
+
 if (!require("choroplethr")) install.packages("choroplethr")
 if (!require("devtools")) install.packages("devtools")
 
+
+# a simple plot of trees count by region
 library(devtools)
 
 if (!require("choroplethrZip")) 
   devtools::install_github('arilamstein/choroplethrZip@v1.5.0')
-
-if (!require("ggplot2")) devtools::install_github("hadley/ggplot2")
-if (!require("ggmap")) devtools::install_github("dkahle/ggmap")
 
 library(choroplethrZip)
 zip_choropleth(count_df,
                title       = "2015 Street Trees Census",
                legend      = "Number of trees",
                county_zoom = 36061)
+
+# problem data set
+problem <- tree 
+
+
+
+
 
